55080cf4b0202376928a5acace41e96962dfc602
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2022-07-16 19:49:03 +0200

    Load and run roms in console

--- Chip8.Console/Chip8Console.cs
+++ Chip8.Console/Chip8Console.cs
@@ -1,18 +1,46 @@
 ﻿using Chip8.Core;
+using System.Text;
+using System.Runtime.InteropServices;
 
 Chip8Core chip8Core = new Chip8Core();
+chip8Core.DisplayChanged += PrintDisplay;
 
-chip8Core.Execute(0x71CC);
-chip8Core.Execute(0x7215);
+if (args.Length == 0)
+{
+    Console.WriteLine("No rom loaded...");
+    PrintUsage();
+    return;
+}
 
+chip8Core.LoadRom(args[0]);
 
 while (true)
 {
-    chip8Core.Execute(0x8124);
+    Thread.Sleep(2);
+    chip8Core.Update();
+}
 
+void PrintDisplay(object sender, EventArgs e)
+{
     Console.Clear();
-    Console.WriteLine($"Register 1: {chip8Core.VariableRegisters[1]}");
-    Console.WriteLine($"Register 2: {chip8Core.VariableRegisters[2]}");
-    Console.WriteLine($"Register F: {chip8Core.VariableRegisters[0xF]}");
-    Console.ReadKey();
+
+    var sb = new StringBuilder();
+    for (int y = 0; y < Chip8Core.DisplayHeight; y++)
+    {
+        for (int x = 0; x < Chip8Core.DisplayWidth; x++)
+        {
+            sb.Append(chip8Core.Display[y, x] ? "#" : " ");
+        }
+        sb.AppendLine();
+    }
+
+    Console.WriteLine(sb.ToString());
+}
+
+void PrintUsage()
+{
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        Console.WriteLine("Usage: Chip8Console.exe <path-to-rom>");
+    else
+        Console.WriteLine("Usage: dotnet run <path-to-rom>");
 }
--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Chip8
-Chip8 interpreters written in C#
+
+Chip8 interpreters written in C# .Net Core 6
 
 ## Chip8.Core
-The core interpreter which handles all the backend work for the interpreters. 
-Written in .Net6
+
+The core interpreter which handles all the backend work for the interpreters.
 
 ## Chip8.Form
+
 Interpreter displaying in a Windows Form.
-Written in .Net6
 
 ## Chip8.Console
-Currently only for testing and debugging.
-Goal is a interpreter displaying in a CLI.
+
+CLI Interpreter, paht to rom as first argument
