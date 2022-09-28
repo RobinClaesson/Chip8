08eae32414090f3ef32d5900e85028e2b60c5ac9
Robin Claesson <robin.claesson@gmail.com>
2022-09-28 19:19:50 +0200

    Added summary on each property of opcodes

--- Chip8.Core/Opcode.cs
+++ Chip8.Core/Opcode.cs
@@ -1,13 +1,37 @@
 ﻿namespace Chip8.Core
 {
+    /// <summary>
+    /// A 2 byte Chip8 Opcode ordered FXYN
+    /// </summary>
     public record Opcode
     {
+        /// <summary>
+        /// First nibble in the opcode 
+        /// </summary>
         public byte First { get; private init; }
-        public byte X { get; private init; }
-        public byte Y { get; private init; }
+        /// <summary>
+        /// Second nibble in the opcode
+        /// </summary>
+        public byte X { get; private init; }     
+        /// <summary>
+        /// Third nibble in the opcode 
+        /// </summary>
+        public byte Y { get; private init; }   
+        /// <summary>
+        /// Fourth (last) nibble in the opcode
+        /// </summary>
         public byte N { get; private init; }
+        /// <summary>
+        /// The second byte (third and fourth nibbles) in the opcode
+        /// </summary>
         public byte NN { get; private init; }
+        /// <summary>
+        /// The second, third and fourth nibbles in the opcode
+        /// </summary>
         public ushort NNN { get; private init; }
+        /// <summary>
+        /// All the four nibbles of the opcodes
+        /// </summary>
         public ushort All { get; private init; }
 
         public Opcode(ushort opcode)
